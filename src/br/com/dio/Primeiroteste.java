aa22bec77ec4e4e835dde3e4e0514610e62bd81b
erinaldo <erinaldoandrade1d2015@gmail.com>
2021-08-16 19:09:40 -0500

    first commit

---
+++ src/br/com/dio/Primeiroteste.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiroteste {
+    public static void main(String[] args) {
+       // System.out.println("Hello World");
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
